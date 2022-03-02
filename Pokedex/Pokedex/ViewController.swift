1bf438daa495b559fd4f6e2b4648c87b9fc75d6f
Jonathas Andrade Torquato <77974117+JonathasTorquato@users.noreply.github.com>
2022-03-02 11:36:06 -0300

    Add files via upload

---
+++ Pokedex/Pokedex/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pokedex
+//
+//  Created by Andrade Torquato, Jonathas on 02/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
