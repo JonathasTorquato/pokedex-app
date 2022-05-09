f4e28f1ce5a11612e587b87c4e648cb988e42799
jsat <jonathas.torquato@gft.com>
2022-05-09 13:10:14 -0300

    [Type] Mudança das cores de apresentação dos tipos

--- Pokedex/Pokedex/UseCase/TypePortuguese.swift
+++ Pokedex/Pokedex/UseCase/TypePortuguese.swift
@@ -17,58 +17,58 @@ class TypePortuguese: TypePortugueseUseCase {
     {
         switch name{
         case "bug":
-            label?.textColor = .systemGreen //OK
+            label?.textColor = UIColor(red: CGFloat(160)/CGFloat(255), green: CGFloat(187)/CGFloat(255), blue: CGFloat(69)/CGFloat(255), alpha: 1) //OK
             return "Inseto"
         case "water":
-            label?.textColor = .systemBlue   //OK
+            label?.textColor = UIColor(red: CGFloat(0)/CGFloat(255), green: CGFloat(70)/CGFloat(255), blue: CGFloat(155)/CGFloat(255), alpha: 1)   //OK
             return "Água"
         case "normal":
             label?.textColor = .systemGray  //OK
             return "Normal"
         case "fighting":
-            label?.textColor = .red     //OK
+            label?.textColor = UIColor(red: CGFloat(255)/CGFloat(255), green: CGFloat(85)/CGFloat(255), blue: CGFloat(28)/CGFloat(255), alpha: 1)     //OK
             return "Lutador"
         case "flying":
             label?.textColor = .systemCyan  //OK
             return "Voador"
         case "poison":
-            label?.textColor = .systemPurple    //OK
+            label?.textColor = UIColor(red: CGFloat(67)/CGFloat(255), green: CGFloat(26)/CGFloat(255), blue: CGFloat(68)/CGFloat(255), alpha: 1)    //OK
             return "Venenoso"
         case "ground":
-            label?.textColor = .systemBrown //OK
+            label?.textColor = UIColor(red: CGFloat(177)/CGFloat(255), green: CGFloat(77)/CGFloat(255), blue: CGFloat(44)/CGFloat(255), alpha: 1) //OK
             return "Terrestre"
         case "rock":
-            label?.textColor = .systemGray2 //OK
+            label?.textColor = UIColor(red: CGFloat(255)/CGFloat(255), green: CGFloat(167)/CGFloat(255), blue: CGFloat(123)/CGFloat(255), alpha: 1) //OK
             return "Pedra"
         case "ghost":
-            label?.textColor = .purple  //OK
+            label?.textColor = UIColor(red: CGFloat(113)/CGFloat(255), green: CGFloat(103)/CGFloat(255), blue: CGFloat(131)/CGFloat(255), alpha: 1)  //OK
             return "Fantasma"
         case "steel":
-            label?.textColor = .blue //OK
+            label?.textColor = UIColor(red: CGFloat(142)/CGFloat(255), green: CGFloat(142)/CGFloat(255), blue: CGFloat(142)/CGFloat(255), alpha: 1) //OK
             return "Metal"
         case "fire":
-            label?.textColor = .systemRed   //OK
+            label?.textColor = UIColor(red: CGFloat(182)/CGFloat(255), green: CGFloat(36)/CGFloat(255), blue: CGFloat(40)/CGFloat(255), alpha: 1)   //OK
             return "Fogo"
         case "grass":
-            label?.textColor = .green   //OK
+            label?.textColor = UIColor(red: CGFloat(39)/CGFloat(255), green: CGFloat(145)/CGFloat(255), blue: CGFloat(55)/CGFloat(255), alpha: 1)   //OK
             return "Planta"
         case "electric":
-            label?.textColor = UIColor(red: 255/255, green: 205/255, blue: 57/255, alpha: 1)    //OK
+            label?.textColor = UIColor(red: 255/255, green: 191/255, blue: 53/255, alpha: 1)    //OK
             return "Elétrico"
         case "psychic":
-            label?.textColor = .magenta   //OK
+            label?.textColor = UIColor(red: CGFloat(176)/CGFloat(255), green: CGFloat(47)/CGFloat(255), blue: CGFloat(123)/CGFloat(255), alpha: 1)   //OK
             return "Psíquico"
         case "ice":
-            label?.textColor = .cyan    //OK
+            label?.textColor = UIColor(red: CGFloat(125)/CGFloat(255), green: CGFloat(228)/CGFloat(255), blue: CGFloat(217)/CGFloat(255), alpha: 1)    //OK
             return "Gelo"
         case "dragon":
-            label?.textColor = .orange
+            label?.textColor = UIColor(red: CGFloat(0)/CGFloat(255), green: CGFloat(39)/CGFloat(255), blue: CGFloat(104)/CGFloat(255), alpha: 1)
             return "Dragão"
         case "dark":
-            label?.textColor = .systemTeal
+            label?.textColor = .label
             return "Noturno"
         case "fairy":
-            label?.textColor = .systemPink
+            label?.textColor = UIColor(red: CGFloat(255)/CGFloat(255), green: CGFloat(165)/CGFloat(255), blue: CGFloat(232)/CGFloat(255), alpha: 1)
             return "Fada"
         case "unknown":
             label?.textColor = .label
