0edd85cffc0066303e2cae3eb533e07aed834597
jsat <jonathas.torquato@gft.com>
2022-05-11 11:19:49 -0300

    [Network] Adição do código de Network no projeto (estava em outra pasta)

---
+++ Pokedex/Pokedex/Network/Network.swift
@@ -0,0 +1,211 @@
+//
+//  Network.swift
+//  Pokedex
+//
+//  Created by Andrade Torquato, Jonathas on 11/05/22.
+//
+
+import Foundation
+import Moya
+
+class Network {
+    
+    fileprivate static let provider = MoyaProvider<API>()
+    
+    static func getPokemonID(id: Int, completion: @escaping(Result<PokemonDTO, Error>)->Void){
+        provider.request(.pokemonId(idPokemon: id)) { result in
+            switch result {
+                
+            case .success(let suc):
+                do {
+                    let decoder = JSONDecoder()
+                    let pokemon = try decoder.decode(PokemonDTO.self, from: suc.data)
+                    completion(.success(pokemon))
+                }catch let error {
+                    completion(.failure(error))
+                }
+            case .failure(let error):
+                completion(.failure(error))
+            }
+        }
+    }
+    
+    static func getPokemonEntry(idPokemon: Int, completion: @escaping(Result<PokedexEntry, Error>)->Void){
+        provider.request(.pokedexEntry(idPokemon: idPokemon)) { result in
+            switch result {
+                
+            case .success(let suc):
+                do {
+                    let decoder = JSONDecoder()
+                    let pokemon = try decoder.decode(PokedexEntry.self, from: suc.data)
+                    completion(.success(pokemon))
+                }catch let error {
+                    completion(.failure(error))
+                }
+            case .failure(let error):
+                completion(.failure(error))
+            }
+        }
+    }
+    
+    static func getPokemonChain(url : String, completion: @escaping(Result<EvolutionChainDTO, Error>)->Void){
+        provider.request(.chainURL(url: url)) { result in
+            switch result {
+                
+            case .success(let suc):
+                do {
+                    let decoder = JSONDecoder()
+                    let pokemon = try decoder.decode(EvolutionChainDTO.self, from: suc.data)
+                    completion(.success(pokemon))
+                }catch let error {
+                    completion(.failure(error))
+                }
+            case .failure(let error):
+                completion(.failure(error))
+            }
+        }
+    }
+    
+    static func getTypeURL(url : String, completion: @escaping(Result<TypeDTO, Error>)->Void){
+        provider.request(.typeURL(url: url.replacingOccurrences(of: Endpoint.baseURL, with: ""))) { result in
+            switch result {
+                
+            case .success(let suc):
+                do {
+                    let decoder = JSONDecoder()
+                    let pokemon = try decoder.decode(TypeDTO.self, from: suc.data)
+                    completion(.success(pokemon))
+                }catch let error {
+                    completion(.failure(error))
+                }
+            case .failure(let error):
+                completion(.failure(error))
+            }
+        }
+    }
+    
+    static func getAllTypes(completion: @escaping(Result<AllTypes, Error>)->Void){
+        provider.request(.types) { result in
+            switch result {
+                
+            case .success(let suc):
+                do {
+                    let decoder = JSONDecoder()
+                    let pokemon = try decoder.decode(AllTypes.self, from: suc.data)
+                    completion(.success(pokemon))
+                }catch let error {
+                    completion(.failure(error))
+                }
+            case .failure(let error):
+                completion(.failure(error))
+            }
+        }
+    }
+    
+    static func getAllItems(completion: @escaping(Result<ItemResults, Error>)->Void){
+        provider.request(.items) { result in
+            switch result {
+                
+            case .success(let suc):
+                do {
+                    let decoder = JSONDecoder()
+                    let pokemon = try decoder.decode(ItemResults.self, from: suc.data)
+                    completion(.success(pokemon))
+                }catch let error {
+                    completion(.failure(error))
+                }
+            case .failure(let error):
+                completion(.failure(error))
+            }
+        }
+    }
+    
+    static func getItemById(id: Int, completion: @escaping(Result<ItemDTO, Error>)->Void){
+        provider.request(.itemId(id: id)) { result in
+            switch result {
+                
+            case .success(let suc):
+                do {
+                    let decoder = JSONDecoder()
+                    let pokemon = try decoder.decode(ItemDTO.self, from: suc.data)
+                    completion(.success(pokemon))
+                }catch let error {
+                    completion(.failure(error))
+                }
+            case .failure(let error):
+                completion(.failure(error))
+            }
+        }
+    }
+    static func getItemByName(name: String, completion: @escaping(Result<ItemDTO, Error>)->Void){
+        provider.request(.itemName(name: name)) { result in
+            switch result {
+                
+            case .success(let suc):
+                do {
+                    let decoder = JSONDecoder()
+                    let pokemon = try decoder.decode(ItemDTO.self, from: suc.data)
+                    completion(.success(pokemon))
+                }catch let error {
+                    completion(.failure(error))
+                }
+            case .failure(let error):
+                completion(.failure(error))
+            }
+        }
+    }
+    
+    static func getCategoryById(id: Int, completion: @escaping(Result<ItemCategory, Error>)->Void){
+        provider.request(.itemCategoryId(id: id)) { result in
+            switch result {
+                
+            case .success(let suc):
+                do {
+                    let decoder = JSONDecoder()
+                    let pokemon = try decoder.decode(ItemCategory.self, from: suc.data)
+                    completion(.success(pokemon))
+                }catch let error {
+                    completion(.failure(error))
+                }
+            case .failure(let error):
+                completion(.failure(error))
+            }
+        }
+    }
+    
+    static func getPokemonCount(completion: @escaping(Result<PokemonCount, Error>)->Void){
+        provider.request(.pokemon) { result in
+            switch result {
+                
+            case .success(let suc):
+                do {
+                    let decoder = JSONDecoder()
+                    let pokemon = try decoder.decode(PokemonCount.self, from: suc.data)
+                    completion(.success(pokemon))
+                }catch let error {
+                    completion(.failure(error))
+                }
+            case .failure(let error):
+                completion(.failure(error))
+            }
+        }
+    }
+    static func getPokemonName(name: String, completion: @escaping(Result<PokemonDTO, Error>)->Void){
+        provider.request(.pokemonName(pokemonName: name)) { result in
+            switch result {
+                
+            case .success(let suc):
+                do {
+                    let decoder = JSONDecoder()
+                    let pokemon = try decoder.decode(PokemonDTO.self, from: suc.data)
+                    completion(.success(pokemon))
+                }catch let error {
+                    completion(.failure(error))
+                }
+            case .failure(let error):
+                completion(.failure(error))
+            }
+        }
+    }
+}
+
